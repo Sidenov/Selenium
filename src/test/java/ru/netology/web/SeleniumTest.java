23147a31a4541c97cef83e1382badbdd4912f8f0
Александр Сиденов <sid_san@mail.ru>
2022-06-01 19:10:48 +0800

    add chromedriver.exe

--- src/test/java/ru/netology/web/SeleniumTest.java
+++ src/test/java/ru/netology/web/SeleniumTest.java
@@ -1,9 +1,14 @@
 package ru.netology.web;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class SeleniumTest {
-    @Test
+
+    @BeforeAll
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver","driver/win/chromedriver.exe");
+    }
 
 
 }
