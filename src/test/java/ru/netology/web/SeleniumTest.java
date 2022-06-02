ce36c1c434611804c07e09ab5a0ea3cf90e342d0
Александр Сиденов <sid_san@mail.ru>
2022-06-02 16:20:04 +0800

    commit

--- src/test/java/ru/netology/web/SeleniumTest.java
+++ src/test/java/ru/netology/web/SeleniumTest.java
@@ -24,7 +24,7 @@ class SeleniumTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
